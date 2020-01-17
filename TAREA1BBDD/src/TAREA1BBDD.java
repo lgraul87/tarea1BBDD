e3eede4df584a1795ab08a896844eed7e5757d27
Raul_Lora_Gomez <lgraul87@gmail.com>
2020-01-17 13:57:19 +0100

    DIAGRAMAS Y SQL

--- TAREA1BBDD/src/TAREA1BBDD.java
+++ TAREA1BBDD/src/TAREA1BBDD.java
@@ -3,7 +3,7 @@ public class TAREA1BBDD {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("TAREA 1 BBDD");
+		System.out.println("TAREA 1 BBDD CAMBIO");
 	}
 
 }
